eaabf6b1667164902865e6fd7e10313c0fbfcb38
dj-soneka <aclecioscruz@gmail.com>
2023-07-06 10:17:00 -0300

    project start

---
+++ src/main/java/br/api/coursewebflux/CourseWebfluxApplication.java
@@ -0,0 +1,13 @@
+package br.api.coursewebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWebfluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/api/coursewebflux/CourseWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.api.coursewebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
