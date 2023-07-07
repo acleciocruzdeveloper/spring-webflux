2aa6d19fe725856d92754a98c39962edccba40c3
dj-soneka <aclecioscruz@gmail.com>
2023-07-06 22:59:15 -0300

    feature(save) implements method save and repository persistence, mapper and check save bd

---
+++ src/main/java/br/api/coursewebflux/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package br.api.coursewebflux.mapper;
+
+import br.api.coursewebflux.entity.User;
+import br.api.coursewebflux.model.request.UserRequest;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import static org.mapstruct.NullValueCheckStrategy.ALWAYS;
+import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
+
+@Mapper(
+        componentModel = "spring",
+        nullValuePropertyMappingStrategy = IGNORE,
+        nullValueCheckStrategy = ALWAYS
+)
+public interface UserMapper {
+    @Mapping(target = "id", ignore = true)
+    User toEntity(final UserRequest request);
+}
---
+++ src/main/java/br/api/coursewebflux/service/UserService.java
@@ -0,0 +1,23 @@
+package br.api.coursewebflux.service;
+
+import br.api.coursewebflux.entity.User;
+import br.api.coursewebflux.mapper.UserMapper;
+import br.api.coursewebflux.model.request.UserRequest;
+import br.api.coursewebflux.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserMapper mapper;
+    private final UserRepository repository;
+
+    public Mono<User> save(final UserRequest request) {
+        return repository.save(mapper.toEntity(request));
+    }
+
+
+}
