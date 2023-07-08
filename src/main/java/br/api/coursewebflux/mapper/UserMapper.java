e4d6b13fce707f3a99efe058be462c2d42d56abc
dj-soneka <aclecioscruz@gmail.com>
2023-07-08 10:20:56 -0300

    method update user

--- src/main/java/br/api/coursewebflux/mapper/UserMapper.java
+++ src/main/java/br/api/coursewebflux/mapper/UserMapper.java
@@ -5,6 +5,7 @@ import br.api.coursewebflux.model.request.UserRequest;
 import br.api.coursewebflux.model.response.UserResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 import static org.mapstruct.NullValueCheckStrategy.ALWAYS;
 import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
@@ -17,6 +18,9 @@ import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
 public interface UserMapper {
     @Mapping(target = "id", ignore = true)
     User toEntity(final UserRequest request);
+
+    @Mapping(target = "id", ignore = true)
+    User toEntity(final UserRequest request, @MappingTarget User entity);
 
     UserResponse toResponse(final User entity);
 }
