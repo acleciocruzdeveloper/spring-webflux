818dea170c0f7f065eb84953ac6b898805c760bb
dj-soneka <aclecioscruz@gmail.com>
2023-07-10 08:13:19 -0300

    add unit test save user service

--- src/main/java/br/api/coursewebflux/controller/UserController.java
+++ src/main/java/br/api/coursewebflux/controller/UserController.java
@@ -3,6 +3,7 @@ package br.api.coursewebflux.controller;
 import br.api.coursewebflux.model.request.UserRequest;
 import br.api.coursewebflux.model.response.UserResponse;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -22,6 +23,7 @@ public interface UserController {
     ResponseEntity<Mono<UserResponse>> update(@PathVariable String id, @RequestBody UserRequest request);
 
     @DeleteMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     ResponseEntity<Mono<Void>> delete(@PathVariable String id);
 
 }
